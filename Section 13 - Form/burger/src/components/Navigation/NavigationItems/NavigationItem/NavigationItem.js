3f755a6803794d85b64c67000d16c8b649e739bb
Roberta <ro.samis.comp@gmail.com>
2020-11-04 17:17:32 -0300

    section 13

---
+++ Section 13 - Form/burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,13 @@
+import classes from './NavigationItem.module.css';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const navigationItem = (props) => (
+  <li className={classes.NavigationItem}>
+    <NavLink to={props.link} activeClassName={classes.active} exact={props.exact}>
+      {props.children}
+    </NavLink>
+  </li>
+);
+
+export default navigationItem;
